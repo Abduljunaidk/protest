8d343f78a51854db3b0454651873d76818768b24
Abdul junaid <69420711+Abduljunaidk@users.noreply.github.com>
2023-08-17 03:13:56 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 // src/routes.js
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import ProductList from './ProductList';
 import AddProductForm from './AddProductForm';
 import ProductItem from './ProductItem';
 
 function Routes() {
   return (
-    <div>
-      <Route path="/" exact component={ProductItem} />
-      <Route path="/add" component={AddProductForm} />
-      <Route path="/item" component={ProductList} />
-    </div>
+    <HashRouter> {/* Wrap the routes with HashRouter */}
+      <div>
+        <Route path="/" exact component={ProductItem} />
+        <Route path="/add" component={AddProductForm} />
+        <Route path="/item" component={ProductList} />
+      </div>
+    </HashRouter>
   );
 }
 
